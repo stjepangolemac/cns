8734a52c293e6345fabb5a22d22950c95f8e6292
Stjepan Golemac <stjepan.golemac@gmail.com>
2016-03-31 17:10:10 +0200

    Delete test.js

--- test.js
+++
@@ -1,8 +0,0 @@
-var caesar = require('./Caesar');
-
-var plain = "if you already have a documentation method that works for you";
-var key = "milevolidiskodisko";
-
-console.log("Plain is:", plain);
-console.log("Cipher is:", caesar.encrypt(plain, key));
-console.log("Deciphered is:", caesar.decrypt(caesar.encrypt(plain, key), key));
